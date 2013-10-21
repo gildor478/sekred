47fb6aaf6f1e9aa67340a1d650821a2fee6f185d
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-10-21 23:07:21 +0200

    Fix ardivink wrt to ounit.
    Ignore-this: b0698de2eb2e21360913b3abb1e79890
    
    darcs-hash:20131021210721-4d692-98b724ec73eb07eef09ad4f9f1fd783a31d7e65a

--- ardivink.lua
+++ ardivink.lua
@@ -4,14 +4,13 @@ ci = require("ci")
 dist = require("dist")
 
 ci.init()
+dist.init()
 oasis.init()
-darcs.init()
 
 ci.prependenv("PATH", "/usr/opt/godi/bin")
 ci.prependenv("PATH", "/usr/opt/godi/sbin")
-ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log.html"))
-ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit.xml"))
-ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log.txt"))
+ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log-$(suite_name).html"))
+ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit-$(suite_name).xml"))
+ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log-$(suite_name)-$(shard_id).txt"))
 
 oasis.std_process("--enable-tests")
-darcs.create_tag(oasis.package_version())
